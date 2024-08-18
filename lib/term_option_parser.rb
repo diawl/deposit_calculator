3aa22b66067c913e369dbfe6ee050cc0ab2b2968
LiangWen <hgxxwl@gmail.com>
2024-08-18 11:21:09 +1000

    Improve option descriptions to make it more clear

--- lib/term_option_parser.rb
+++ lib/term_option_parser.rb
@@ -46,25 +46,25 @@ class TermOptionParser
 
   def define_options
     parser.banner = "Usage: deposit_calculator [options]"
-    parser.on("-dDeposit", "--deposit=DEPOSIT", Integer, "Initial deposit, integer only") do |d|
+    parser.on("-dDeposit", "--deposit=DEPOSIT", Integer, "Initial deposit, integer only, must be between 1,000 and 1,500,000") do |d|
       if d < MIN_DEPOSIT || d > MAX_DEPOSIT
         raise OptionParser::InvalidArgument, "Deposit must be between 1,000 and 1,500,000."
       end
       profile.deposit = d
     end
-    parser.on("-rRate", "--rate=RATE", Float, "Percentage annual interest rate") do |r|
+    parser.on("-rRate", "--rate=RATE", Float, "Percentage annual interest rate, must be positive") do |r|
       if r < 0
         raise OptionParser::InvalidArgument, "Interest rate must be positive"
       end
       profile.rate = r
     end
-    parser.on("-tTerm", "--term=TERM", Integer, "Investment term in months") do |t|
+    parser.on("-tTerm", "--term=TERM", Integer, "Investment term in months, integer only, must be between 3 and 60") do |t|
       if t < MIN_TERM || t > MAX_TERM
         raise OptionParser::InvalidArgument, "Term must be between 3 and 60 months"
       end
       profile.term = t
     end
-    parser.on("-cCycle", "--cycle=Cycle", "Interest payment cycle") do |c|
+    parser.on("-cCycle", "--cycle=CYCLE", "Interest payment cycle, must be one of: #{VALID_CYCLE.join(', ')}") do |c|
       if VALID_CYCLE.include?(c)
         profile.cycle = c
       else
