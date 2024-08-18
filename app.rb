681c83bb1740b7132c3fc678ab400b8c4e97971d
LiangWen <hgxxwl@gmail.com>
2024-08-18 11:09:04 +1000

    handle error returned by calculator

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require_relative 'lib/term_calculator'
 require_relative 'lib/term_option_parser'
 
-option_parser = TermOptionParser.new
-option_parser.parse!(ARGV)
-
-puts "$#{TermCalculator.new(option_parser.profile).calculate}"
+begin
+  option_parser = TermOptionParser.new
+  option_parser.parse!(ARGV)
+  puts "$#{TermCalculator.new(option_parser.profile).calculate}"
+rescue StandardError => error
+  puts "Error: #{error.message}"
+end
